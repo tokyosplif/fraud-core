721c484db9a0cf45e6208ef06669c2a4f0a0c3aa
oleg <tokyospliff3@gmail.com>
2026-02-22 23:35:53 +0300

    add SafeClose utility

---
+++ pkg/closer/closer.go
@@ -0,0 +1,15 @@
+package closer
+
+import (
+	"io"
+	"log/slog"
+)
+
+func SafeClose(c io.Closer, name string) {
+	if c == nil {
+		return
+	}
+	if err := c.Close(); err != nil {
+		slog.Warn("Failed to close resource", "resource", name, "err", err)
+	}
+}
