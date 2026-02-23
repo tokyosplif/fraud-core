52ccd112a5e2bad6ce39135db1e13a5f6d137134
oleg <tokyospliff3@gmail.com>
2026-02-23 23:58:40 +0300

    update logger: add configurable log level to Setup function

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,17 +3,29 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"strings"
 )
 
-func Setup() *slog.Logger {
+func Setup(level string) {
+	var slogLevel slog.Level
+
+	switch strings.ToLower(level) {
+	case "debug":
+		slogLevel = slog.LevelDebug
+	case "warn":
+		slogLevel = slog.LevelWarn
+	case "error":
+		slogLevel = slog.LevelError
+	default:
+		slogLevel = slog.LevelInfo
+	}
+
 	opts := &slog.HandlerOptions{
-		Level: slog.LevelInfo,
+		Level: slogLevel,
 	}
 
 	handler := slog.NewJSONHandler(os.Stdout, opts)
 	logger := slog.New(handler)
 
 	slog.SetDefault(logger)
-
-	return logger
 }
