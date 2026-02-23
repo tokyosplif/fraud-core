679644fb0ecb6ca1bf972218d5db8acefeb8fac1
oleg <tokyospliff3@gmail.com>
2026-02-23 15:30:49 +0300

    replace log with slog

--- cmd/processor/main.go
+++ cmd/processor/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"context"
-	"log"
+	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/tokyosplif/fraud-core/internal/app"
+	"github.com/tokyosplif/fraud-core/pkg/logger"
 )
 
 func main() {
+	logger.Setup()
+
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
+	slog.Info("Starting Fraud Processor Service...")
+
 	if err := app.RunProcessor(ctx); err != nil {
-		log.Fatalf("Processor stopped with error: %v", err)
+		slog.Error("Processor fatal error", "err", err)
+		os.Exit(1)
 	}
+
+	slog.Info("Processor stopped gracefully")
 }
