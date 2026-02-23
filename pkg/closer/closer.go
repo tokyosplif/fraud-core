4a53a7899ee595c678d5ad89cb7b3f580a07c24a
oleg <tokyospliff3@gmail.com>
2026-02-23 23:58:31 +0300

    refactor closer: rename SafeClose to Close and update logging for errors

--- pkg/closer/closer.go
+++ pkg/closer/closer.go
@@ -5,13 +5,14 @@ import (
 	"log/slog"
 )
 
-func SafeClose(c io.Closer, name string) {
+func Close(c io.Closer, name string) {
 	if c == nil {
 		return
 	}
 	if err := c.Close(); err != nil {
-		slog.Warn("Failed to close resource", "resource", name, "err", err)
-	} else {
-		slog.Debug("Resource closed successfully", "resource", name)
+		slog.Error("failed to close resource",
+			"resource", name,
+			"error", err,
+		)
 	}
 }
