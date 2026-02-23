c47576a69f3a0efbba4e08b137544838cfc63389
oleg <tokyospliff3@gmail.com>
2026-02-23 15:32:15 +0300

    add MaxTx and AvgTx fields to User struct

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -4,9 +4,11 @@ import "time"
 
 type User struct {
 	ID        string       `gorm:"primaryKey"`
-	Email     string       `gorm:"size:255;default:'unknown@mail.com'"` // Додаємо size для рядків
+	Email     string       `gorm:"size:255;default:'unknown@mail.com'"`
 	RiskScore int          `gorm:"default:10"`
 	IsBanned  bool         `gorm:"default:false"`
+	MaxTx     float64      `gorm:"-"`
+	AvgTx     float64      `gorm:"-"`
 	Events    []FraudEvent `gorm:"foreignKey:UserID"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
