c277ffba1372d7d9382df5ba9d8bafe38e3eab24
oleg <tokyospliff3@gmail.com>
2026-02-23 22:37:54 +0300

    update FraudEvent

--- internal/domain/fraud_event.go
+++ internal/domain/fraud_event.go
@@ -5,12 +5,12 @@ import "time"
 type FraudEvent struct {
 	ID            uint    `gorm:"primaryKey"`
 	TransactionID string  `gorm:"uniqueIndex;not null;size:100"`
-	UserID        string  `gorm:"index;not null;size:100"` // Зв'язок з User.ID
+	UserID        string  `gorm:"index;not null;size:100"`
 	Merchant      string  `gorm:"size:255"`
-	Amount        float64 `gorm:"type:decimal(10,2)"` // Для грошей краще використовувати decimal
+	Amount        float64 `gorm:"type:decimal(10,2)"`
 	Location      string  `gorm:"size:255"`
 	IsBlocked     bool
-	AIReason      string    `gorm:"type:text"` // Для довгих пояснень ШІ
+	AIReason      string    `gorm:"type:text"`
 	AIPushMsg     string    `gorm:"type:text"`
 	CreatedAt     time.Time `gorm:"autoCreateTime"`
 }
